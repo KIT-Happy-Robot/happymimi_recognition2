731b5216e86acdcdab68e5bdb473bda37caa3de6
takuchan <takuchanapp@gmail.com>
2024-10-23 14:18:05 +0900

    ローンチファイルがいらなかったので削除

--- recognition_launch/README.md
+++
@@ -1,7 +0,0 @@
-# normallaunch.py
-ROS2とOpenCVが正しく接続されているかを確認できるラウンチファイル。
-`$ ros2 launch recognition_launch normallaunch.py`
-## usage
-ウェブカメラを用いるすべてのノードを立ち上げるラウンチファイルです。
-上記のコマンドを入力したら、`rqt`と`rqt_graph`を別ターミナルウィンドウで叩き、トピック通信で流れているカメラ映像を取得しましょう。
-
--- recognition_startup/setup.py
+++ recognition_startup/setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages, setup
-import os 
-from glob import glob
 
-package_name = 'recognition_launch'
+package_name = 'recognition_startup'
 
 setup(
     name=package_name,
@@ -12,12 +10,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='tk',
-    maintainer_email='takuchanpp@gmail.com',
+    maintainer_email='takuchanapp@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
